a0b97832cce73b284702a1f8676af88e4db53095
Callum Wallach <callum.wallach@gmail.com>
2023-05-19 18:41:51 +1000

    modified ui formatting

--- loading.js
+++ loading.js
@@ -1,7 +1,7 @@
 class Loading {
   constructor(game) {
     this.game = game;
-    this.fontSize = 40;
+    //this.fontSize = 40;
     this.fontFamily = "Creepster";
   }
   update() {}
@@ -17,16 +17,20 @@ class Loading {
     context.shadowBlur = 0;
     context.fillStyle = this.game.fontColor;
     context.textAlign = "center";
-    context.font = `${this.fontSize * 3}px ${this.fontFamily}`;
+    //context.font = `${this.fontSize * 3}px ${this.fontFamily}`;
+    context.font = `7rem ${this.fontFamily}`;
+
     const heading = `Anya vs The Night`;
-    const message1 = `It's Anya's birthday... tomorrow she will be 8`;
-    const message2 = `But first... she must battle the forces of evil!!`;
-    const message3 = `Will she survive the night??!!`;
+    const message1 = `It's Anya's birthday tomorrow... she will be 8`;
+    const message2 = `But first... she must battle the forces of the night!!`;
+    const message3 = `Will she survive and make it to her birthday??!!`;
     context.fillText(heading, gw * 0.5, gh * 0.5 - 50);
-    context.font = `${this.fontSize * 0.9}px ${this.fontFamily}`;
+    //context.font = `${this.fontSize * 0.9}px ${this.fontFamily}`;
+    context.font = `2rem ${this.fontFamily}`;
     context.fillText(message1, gw * 0.5, gh * 0.5 + 20);
     context.fillText(message2, gw * 0.5, gh * 0.5 + 70);
-    context.font = `${this.fontSize * 1.25}px ${this.fontFamily}`;
+    //context.font = `${this.fontSize * 1.25}px ${this.fontFamily}`;
+    context.font = `3rem ${this.fontFamily}`;
     context.fillText(message3, gw * 0.5, gh * 0.5 + 140);
     context.restore();
   }
