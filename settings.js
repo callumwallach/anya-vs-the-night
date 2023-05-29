a20d57f425a58569e50abd82e740f13ba210fb60
Callum Wallach <callum.wallach@gmail.com>
2023-05-29 13:22:07 +1000

    updated loading message

--- settings.js
+++ settings.js
@@ -40,23 +40,56 @@ export default {
     character: "girl",
     title: "Anya is Awesome!!",
     loading: [
-      { fontSize: 2.5, message: "Anya vs The Night", position: -50 },
       {
-        fontSize: 0.75,
-        message: "It's Anya's birthday tomorrow... she will be 8",
-        position: +10,
+        fontSize: 1.5,
+        message: "Happy 8th birthday Anya!!",
+        position: -135,
       },
       {
         fontSize: 0.75,
-        message: "But first... she must battle the forces of the night!!",
-        position: +50,
+        message: "Your lil buddha belly is probably full of cake and junk food",
+        position: -85,
       },
       {
-        fontSize: 1,
-        message: "Will she survive and make it to her birthday??!!",
-        position: +110,
+        fontSize: 0.75,
+        message: "But before you relax in front of a birthday movie",
+        position: -45,
+      },
+      {
+        fontSize: 0.75,
+        message: "You must defeat my minions and survive the night!!",
+        position: -5,
+      },
+      {
+        fontSize: 1.25,
+        message: "Welcome to...",
+        position: 55,
+      },
+      { fontSize: 2.5, message: "Anya vs The Night!!", position: 150 },
+      {
+        fontSize: 0.5,
+        message: "( I built this game just for you!! )",
+        position: +190,
       },
     ],
+    // loading: [
+    //   { fontSize: 2.5, message: "Anya vs The Night", position: -50 },
+    //   {
+    //     fontSize: 0.75,
+    //     message: "It's Anya's birthday tomorrow... she will be 8",
+    //     position: +10,
+    //   },
+    //   {
+    //     fontSize: 0.75,
+    //     message: "But first... she must battle the forces of the night!!",
+    //     position: +50,
+    //   },
+    //   {
+    //     fontSize: 1,
+    //     message: "Will she survive and make it to her birthday??!!",
+    //     position: +110,
+    //   },
+    // ],
     end: {
       success: [
         { fontSize: 1.2, message: "You did it!!!", position: -115 },
